c5fca2871d0dff52668aa74d1fbba835afddfc42
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-08 09:07:28 +0200

    IComparable

---
+++ InterfaceIComparable/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceICloneable
+{
+    class Person : IComparable<Person>
+    {
+        public Person(string name, string surnames, int age)
+        {
+            Name = name;
+            Surnames = surnames;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public string Surnames { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person? other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        public override string ToString()
+        {
+            return $"Person(Name: {Name}, Surnames: {Surnames}, Age: {Age})";
+        }
+    }
+}
---
+++ InterfaceIComparable/Program.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceICloneable;
+
+Person person1 = new Person("CFernando", "Carrillo", 18);
+Person person2 = new Person("AJuan", "Carrillo", 18);
+Person person3 = new Person("DFrank", "Carrillo", 18);
+Person person4 = new Person("BMike", "Carrillo", 18);
+
+Person[] people = new Person[] { person1, person2, person3, person4 };
+
+foreach (var item in people)
+{
+    Console.WriteLine(item);
+}
+
+Array.Sort(people);
+Console.WriteLine("--------------------------------");
+
+foreach (var item in people)
+{
+    Console.WriteLine(item);
+}
