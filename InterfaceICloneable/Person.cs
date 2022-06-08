54f74a4f8866b1fc55634e4b4786661bc734ae74
Fernando Carrillo Lopez <kleyfat12@gmail.com>
2022-06-08 08:45:23 +0200

    ICloneable

---
+++ InterfaceICloneable/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceICloneable
+{
+    class Person : ICloneable
+    {
+        public Person(string name, string surnames, int age)
+        {
+            Name = name;
+            Surnames = surnames;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public string Surnames { get; set; }
+        public int Age { get; set; }
+
+        public object Clone()
+        {
+            return new Person(Name, Surnames, Age);
+        }
+
+        public override string ToString()
+        {
+            return $"Person(Name: {Name}, Surnames: {Surnames}, Age: {Age})";
+        }
+    }
+}
---
+++ InterfaceICloneable/Program.cs
@@ -0,0 +1,29 @@
+﻿using InterfaceICloneable;
+
+Person person = new Person("Fernando", "Carrillo", 18);
+
+// This is not a clone
+Person person2 = person;
+
+Console.WriteLine(person);
+Console.WriteLine(person2);
+
+person.Name = "Juan";
+
+Console.WriteLine(person);
+Console.WriteLine(person2);
+// person and person2 point to the same memory space
+
+Console.WriteLine("------------------------------------");
+
+person2 = (Person)person.Clone();
+// Now person 2 point to a different memory space
+Console.WriteLine(person);
+Console.WriteLine(person2);
+
+// If I change something in person
+person.Name = "Cesar";
+
+// There is no change in person2
+Console.WriteLine(person);
+Console.WriteLine(person2);
